f548f4d3e799d1123dea0f8900b775bd2bec8cdd
Dinael URDANETA <dinael@gmail.com>
2023-08-22 08:07:10 +0200

    fix: add conditions to formatdate

--- src/utils/DateFormat.jsx
+++ src/utils/DateFormat.jsx
@@ -1,18 +1,21 @@
 function formatDate(rawDate) {
-    const date = new Date(rawDate)
-    const day = date.getDate()
-    const month = date.getMonth() + 1
-    const year = date.getFullYear()
+    const date = new Date(rawDate);
 
-    return `${day}/${month}/${year}`
+    if (isNaN(date)) {
+        return 'dd/mm/yy';
+    }
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
 }
 
 function DateFormatted({ date }) {
-    const formattedDate = formatDate(date)
+    const formattedDate = formatDate(date);
 
-    return (
-        <>{formattedDate}</>
-    )
+    return <>{formattedDate}</>;
 }
 
-export default DateFormatted
+export default DateFormatted;
